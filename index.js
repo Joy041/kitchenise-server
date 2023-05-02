6b651336702ed506aec7b38d9293c6939382de3e
Joy Purkistha <joypurokisthosh@gmail.com>
2023-05-02 14:23:03 +0600

    chedid and cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 5000
 
 const chef = require('./data/chefData.json')
+
+app.use(cors())
 
 
 app.get('/', (req, res) => {
@@ -11,6 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/chef', (req, res) => {
   res.send(chef)
+})
+
+app.get('/chef/:id', (req, res) => {
+  const id = req.params.id;
+  const chefId = chef.find(c => c._id === id);
+  res.send(chefId)
 })
 
 app.listen(port, () => {
