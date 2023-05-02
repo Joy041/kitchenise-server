5a53f5571f3ae866ab1aa173ffc69e8c644cf6b6
Joy Purkistha <joypurokisthosh@gmail.com>
2023-05-02 14:17:45 +0600

    send chefdata

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-
+const chef = require('./data/chefData.json')
 
 
 app.get('/', (req, res) => {
   res.send('Chefs server')
+})
+
+app.get('/chef', (req, res) => {
+  res.send(chef)
 })
 
 app.listen(port, () => {
